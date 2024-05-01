ef364ddc8d2a7d50f1473bf8cd1c4af45b01d5a3
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-01 13:37:15 +0530

    Responsive homepage

--- scipt.js
+++ scipt.js
@@ -2,7 +2,7 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
 
-menuIcon.onclick = function(){
+menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 }
